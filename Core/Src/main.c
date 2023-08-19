14513ddd8bd76b320658edefd8ae010674f27384
Rojar Smith <rojarsmith@gmail.com>
2023-08-19 09:33:44 +0800

    Fix Implicit Warning

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -26,6 +26,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <stdio.h>
 #include "mbedtls/sha1.h"
 /* USER CODE END Includes */
 
